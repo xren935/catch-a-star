07eb0aba44cc174f0aeadc707b4801de949564ee
xren935 <59517115+xren935@users.noreply.github.com>
2021-03-20 09:52:16 -0700

    Header + Footer ✅

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2020 NPM-Star. All Rights Reserved. Made by Annie Ren.
+            </footer>
+        );
+    }
+}
+
+export default Footer;
