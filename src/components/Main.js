2b538d7455aeb370727f4470e3811008b7bbea11
xren935 <59517115+xren935@users.noreply.github.com>
2021-04-02 21:40:51 -0700

    why are my tracks not showing upppp

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,79 +1,151 @@
-import React, { Component } from "react";
-import { Row, Col } from "antd";
-import SatSetting from "./SatSetting";
-import SatelliteList from "./SatelliteList";
-import axios from 'axios';
-import {NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "../constant";
+// import React, { Component } from "react";
+// import { Row, Col } from "antd";
+// import SatSetting from "./SatSetting";
+// import SatelliteList from "./SatelliteList";
+// import axios from 'axios';
+// import {NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "../constant";
+// import WorldMap from './WorldMap';
 
+// // sample data 
+// // 70; -40; 90; 90; 2
+
+// class Main extends Component {
+//   constructor(){
+//       super();
+//       this.state = {
+//           satInfo: null,
+//           settings: null,
+//           setting: null,
+//           isLoadingList: false
+//       };
+//   }
+
+//   showNearbySatellite = (setting) => {
+//     this.setState({
+//         settings: setting
+//     })
+//     this.fetchSatellite(setting);
+//   };
+
+//   fetchSatellite= (setting) => {
+//     const {latitude, longitude, elevation, altitude} = setting;
+//     const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+ 
+//     this.setState({
+//       isLoadingList: true
+//     });
+
+//     axios.get(url)
+//       .then(response => {
+//           console.log(response.data)
+//           this.setState({
+//               satInfo: response.data,
+//               isLoadingList: false
+//           });
+//       })
+//       .catch(error => {
+//           console.log('err in fetch satellite -> ', error);
+//       });
+//     };
+
+//     showMap = selected => {
+//       this.setState(preState => ({
+//         ...preState,
+//         isLoadingMap: true,
+//         satList: [...selected]
+//       }));
+//     };
+
+//     render() {
+//       const { isLoadingList, satInfo, satList, setting } = this.state;
+//       return (
+//         <Row className="main">
+//           <Col span={8} className="left-side">
+//             <SatSetting onShow={this.showNearbySatellite} />
+//             <SatelliteList
+//               satInfo={satInfo}
+//               isLoad={this.state.isLoadingList}
+//               onShowMap={this.showMap}
+//             />
+//           </Col>
+//           <Col span={16} className="right-side">
+//             <WorldMap satData={satList} observerData={setting} />
+//           </Col>
+//         </Row>
+//       );
+//     }
+// }
+
+// export default Main;
+import React, {Component} from 'react';
+import { Row, Col } from 'antd';
+import axios from 'axios';
+import { NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY } from '../constant';
+import SatSetting from './SatSetting';
+import SatelliteList from './SatelliteList';
 import WorldMap from './WorldMap';
 
-// sample data 
-// 70; -40; 90; 90; 2
-
 class Main extends Component {
-  constructor(){
-      super();
-      this.state = {
-          satInfo: null,
-          settings: null,
-          isLoadingList: false
-      };
-  }
-
-  showNearbySatellite = (setting) => {
-    this.setState({
-        settings: setting
-    })
-    this.fetchSatellite(setting);
-  };
-
-  fetchSatellite= (setting) => {
-    const {latitude, longitude, elevation, altitude} = setting;
-    const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
- 
-    this.setState({
-      isLoadingList: true
-    });
-
-    axios.get(url)
-      .then(response => {
-          console.log(response.data)
-          this.setState({
-              satInfo: response.data,
-              isLoadingList: false
-          });
-      })
-      .catch(error => {
-          console.log('err in fetch satellite -> ', error);
-      });
-    };
-
-    showMap = selected => {
-      this.setState(preState => ({
-        ...preState,
-        isLoadingMap: true,
-        satList: [...selected]
-      }));
-    };
-
+    constructor(){
+        super();
+        this.state = {
+            satInfo: null,
+            satList: null,
+            setting: null,
+            isLoadingList: false
+        }
+    }
     render() {
-      const { satInfo } = this.state;
-      return (
-        <Row className="main">
-          <Col span={8} className="left-side">
-            <SatSetting onShow={this.showNearbySatellite} />
-            <SatelliteList
-              satInfo={satInfo}
-              isLoad={this.state.isLoadingList}
-              onShowMap={this.showMap}
-            />
-          </Col>
-          <Col span={16} className="right-side">
-            <WorldMap />
-          </Col>
-        </Row>
-      );
+        const { isLoadingList, satInfo, satList, setting } = this.state;
+        return (
+            <Row className='main'>
+                <Col span={8} >
+                    <SatSetting onShow={this.showNearbySatellite}/>
+                    <SatelliteList isLoad={isLoadingList}
+                                   satInfo={satInfo}
+                                   onShowMap={this.showMap} />
+                </Col>
+                <Col span={16} className="right-side">
+                    <WorldMap satData={satList} observerData={setting} />
+                </Col>
+            </Row>
+        );
+    }
+
+    showMap = (selected) => {
+        this.setState(preState => ({
+            ...preState,
+            satList: [...selected]
+        }))
+    }
+
+    showNearbySatellite = (setting) => {
+        this.setState({
+            isLoadingList: true,
+            setting: setting
+        })
+        this.fetchSatellite(setting);
+    }
+
+    fetchSatellite= (setting) => {
+        const {latitude, longitude, elevation, altitude} = setting;
+        const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+
+   this.setState({
+       isLoadingList: true
+   });
+
+   axios.get(url)
+       .then(response => {
+           console.log(response.data)
+           this.setState({
+               satInfo: response.data,
+               isLoadingList: false
+           })
+       })
+       .catch(error => {
+           console.log('err in fetch satellite -> ', error);
+       })
     }
 }
-
 export default Main;
--- src/components/SatelliteList.js
+++ src/components/SatelliteList.js
@@ -1,30 +1,165 @@
+// import React, {Component} from 'react';
+// import { List, Avatar, Button, Checkbox, Spin } from 'antd';
+// import satellite from "../assets/images/satellite.svg";
+
+// class SatelliteList extends Component {
+//     constructor(){
+//         super();
+//         this.state = {
+//             selected: [],
+//             isLoaded: false
+//         };
+//     }
+
+//     onShowSatMap = () =>{
+//         this.props.onShowMap(this.state.selected);
+//     }
+
+//     render() {
+//         const satList = this.props.satInfo ? this.props.satInfo.above : [];
+//         const { isLoaded } = this.props;
+//         const { selected } = this.state;
+//         console.log(this.state);
+
+//         return (
+//             <div className="sat-list-box">
+//                 <Button className="sat-list-btn" onClick={this.onShowSatMap} disabled={selected.length === 0}>
+//                     Track on the map
+//                 </Button>
+//                 <hr/>
+
+//                 {
+//                     isLoaded ?
+//                         <div className="spin-box">
+//                             <Spin tip="Loading..." size="large" />
+//                         </div>
+//                         :
+//                         <List
+//                             className="sat-list"
+//                             itemLayout="horizontal"
+//                             size="small"
+//                             dataSource={satList}
+//                             renderItem={item => (
+//                                 <List.Item
+//                                     actions={[<Checkbox dataInfo={item} onChange={this.onChange}/>]}
+//                                 >
+//                                     <List.Item.Meta
+//                                         avatar={<Avatar size={50} src={satellite} />}
+//                                         title={<p>{item.satname}</p>}
+//                                         description={`Launch Date: ${item.launchDate}`}
+//                                     />
+
+//                                 </List.Item>
+//                             )}
+//                         />
+//                 }
+//             </div>
+//         );
+//     }
+
+
+//     // showMap = () => {
+//     //     const { selected } = this.state;
+//     //     this.props.onShowMap(selected);
+//     // }
+
+//     // adding/removing sats from the list
+//     addOrRemove = (sat, status, list) => {
+//         // check if it's already in selected 
+//             // add if status is true and is not in list 
+//         // use array.some(item => item == value) to determin if sat is in list 
+//         const found = list.some(item => item.satid === sat.saitid);
+//         if(status && !found){
+//             // add to the list 
+//             list = [...list, sat];
+//         }
+//         if(!status && found){
+//             // remove from the list 
+//             list = list.filter(item => item.satid !== sat.satid);
+//         }
+//         return list;
+//     }
+
+//     // for the checkbox 
+//     onChange = e => {
+//         // get datainfo and checkbox status 
+//         const {dataInfo, checked } = e.target; 
+
+//         // add/remove selected satellite to/from selected array
+//         const { selected } = this.state; 
+//         const list = this.addOrRemove(dataInfo, checked, selected); 
+
+//         // update by se{tting the state 
+//         this.setState({
+//             selected: list
+//         })
+//     }
+
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         if(prevProps.satInfo !== this.props.satInfo) {
+//             this.setState({selected: []})
+//         }
+//     }
+// }
+
+// export default SatelliteList;
 import React, {Component} from 'react';
 import { List, Avatar, Button, Checkbox, Spin } from 'antd';
 import satellite from "../assets/images/satellite.svg";
 
 class SatelliteList extends Component {
-    // constructor(){
-    //     super();
-    //     this.state = {
-    //         selected: [],
-    //         isLoad: false
-    //     };
-    // }
-    state = {
-        selected: []
+    constructor(){
+        super();
+        this.state = {
+            selected: [],
+            isLoad: false
+        };
+    }
+
+    onChange = e => {
+        const { dataInfo, checked } = e.target;
+        const { selected } = this.state;
+        const list = this.addOrRemove(dataInfo, checked, selected);
+        this.setState({ selected: list })
+    }
+
+    addOrRemove = (item, status, list) => {
+        const found = list.some( entry => entry.satid === item.satid);
+        if(status && !found){
+            list.push(item)
+        }
+
+        if(!status && found){
+            list = list.filter( entry => {
+                return entry.satid !== item.satid;
+            });
+        }
+        return list;
+    }
+
+    onShowSatMap = () =>{
+        console.log("show track");
+        console.log(this.state.selected);
+        this.props.onShowMap(this.state.selected);
     }
 
     render() {
         const satList = this.props.satInfo ? this.props.satInfo.above : [];
         const { isLoad } = this.props;
         const { selected } = this.state;
-        console.log(this.state);
 
         return (
             <div className="sat-list-box">
-                <Button className="sat-list-btn" onClick={this.onShowSatOnMap} disabled={selected.length === 0}>
-                    Track on the map
-                </Button>
+                <div className="btn-container">
+                   <Button
+                     className="sat-list-btn"
+                     size="large"
+                     disabled={selected.length === 0}
+                     onClick={this.onShowSatMap}
+                   >
+            Track on the map
+          </Button>
+        </div>
                 <hr/>
 
                 {
@@ -54,50 +189,6 @@ class SatelliteList extends Component {
                 }
             </div>
         );
-    }
-
-
-    showMap = () => {
-        const { selected } = this.state;
-        this.props.onShowMap(selected);
-    }
-
-    // adding/removing sats from the list
-    addOrRemove = (sat, status, list) => {
-        // check if it's already in selected 
-            // add if status is true and is not in list 
-        // use array.some(item => item == value) to determin if sat is in list 
-        const found = list.some(item => item.satid === sat.saitid);
-        if(status && !found){
-            // add to the list 
-            list = [...list, sat];
-        }
-        if(!status && found){
-            // remove from the list 
-            list = list.filter(item => item.satid !== sat.satid);
-        }
-        return list;
-    }
-
-    // for the checkbox 
-    onChange = e => {
-        // get datainfo and checkbox status 
-        const {dataInfo, checked } = e.target; 
-
-        // add/remove selected satellite to/from selected array
-        const { selected } = this.state; 
-        const list = this.addOrRemove(dataInfo, checked, selected); 
-
-        // update by se{tting the state 
-        this.setState({
-            selected: list
-        })
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if(prevProps.satInfo !== this.props.satInfo) {
-            this.setState({selected: []})
-        }
     }
 }
 
